cb0d323e53eee0e31fc7150a52ac791885ca969c
Jonathan Ribeiro <147669492+JonathanRbo@users.noreply.github.com>
2026-03-18 14:34:37 -0300

    Delete APPC directory

--- APPC/aula-02-03-2026/celsius_para_fahrenheit.py
+++
@@ -1,9 +0,0 @@
-print("Programa para converter graus Celsius em graus Fahrenheit\n")
-
-celsius = float(input("Digite a temperatura em Celsius: "))
-
-fahrenheit = (celsius * 9/5) + 32
-
-print(f"\n{celsius}°C equivale a {fahrenheit}°F")
-
-print("\nPrograma encerrado com sucesso")
